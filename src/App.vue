7c9e275ed642b7614d29a342cf5699925eb7066a
jazz <chin.r@qq.com>
2025-07-22 19:34:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Person/>
+</template>
+
+<script lang="ts">
+import Person from "./components/Person.vue";
+
+export default {
+  name: "App", //组件名
+  components: { Person },
+};
+</script>
+
---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>address: {{ address }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">年龄+1</button>
+    <button @click="showTel">点我查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Person",
+};
+</script>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+let name = ref('fiona')
+let age = ref(35)
+let address = 'LiangNing Dalian'
+let tel = '13612345678';
+
+function changeName() {
+  name.value = 'zhang san';
+  console.log(name);
+}
+
+function changeAge() {
+  age.value += 1;
+  console.log(age);
+}
+
+function showTel() {
+  alert(tel)
+}
+</script>
+<style>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
