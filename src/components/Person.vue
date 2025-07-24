3f1d8720c8cdccadf96bc12304c79f38244a5b8a
jazz <chin.r@qq.com>
2025-07-25 06:47:19 +0800

    computed

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="person">
-<h2>One car name is {{car.brand}}, price is {{car.price}}</h2>
-    <button @click="changePrice">change price</button>
-    <ul>
-    <li v-for="p in ps" v-bind:key="p.id">{{p.name}}</li>
-    </ul>
+    name :<input id="name" type="text" v-model="name" /> <br/>
+    age: <input id="age" type="text" v-model="age" ><br/>
+    full name: {{ fullname}}<br/>
 
     <button @click="changeName">change Name</button>
-
-    <hr>
-    <h2>{{obj.a.b.c.name}}</h2>
-    <button @click="changeObj">change price</button>
+    <button @click="changeFullName">change Full Name</button>
   </div>
 </template>
 
@@ -21,47 +16,33 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import {ref, reactive} from 'vue'
+import {ref, reactive, toRefs, computed} from 'vue'
 defineOptions({
   name: 'Person'
 })
 // data
-let car = ref({brand:'benz', price:100})
-let ps = ref([
-  {id:'f01', name:'June'},
-  {id:'f02', name:'Fiona'},
-  {id:'f03', name:'Monica'}
-])
+let props = reactive({name:'fiona', age:35})
+let {name,age} = toRefs(props)
+let fullname = computed({
+  get(){
+    return name.value +'-'+ age.value
+  },
+  set(n){
+    console.log(n)
+    let [n1,n2] = n.split('-')
+    name.value = n1
+    age.value = parseInt(n2)
 
-console.log(car)
-console.log(ps)
-let obj = reactive({
-  a:{
-    b:{
-      c:{
-        name:666
-      }
-    }
   }
 })
 
-// methods
-
-function changePrice() {
-  //car.price += 10;
- // car.value.price += 10;
- //  car = Object.assign(car, {brand:'Audi', price:200})
-  car.value = {brand:'Toyota', price:50}
+ // function
+function changeName(){
+  name.value += '~'
 }
-
-function changeName() {
-  ps.value[0].name = 'Luna'
+function changeFullName(){
+  name.value = 'Monica-35'
 }
-
-function changeObj() {
-  obj.a.b.c.name = 999
-}
-
 </script>
 <style>
 .person {
