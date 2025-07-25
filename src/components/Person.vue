2900217a6053c27d245e6757cd36820c369570b6
jazz <chin.r@qq.com>
2025-07-25 21:17:27 +0800

    watch ref/reactive 定义的对象类型中的某个属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="person">
-    <h1> ref, 对象类型的数据</h1>
-    <h2>Name: {{ p.name }} </h2>
-    <h2>Name: {{ p.age }} </h2>
+    <h1> ref,reactive 对象类型的某个属性</h1>
+    <h2>Name: {{ person.name }} </h2>
+    <h2>Name: {{ person.age }} </h2>
+    <h2>Car 1: {{ person.car.c1 }} </h2>
+    <h2>Car 2: {{ person.car.c2 }} </h2>
 
     <button @click="changeName">Change Name</button>
     <button @click="changeAge">Change Age</button>
-    <button @click="changePerson">Change Person</button>
+    <button @click="changeC1">Change C1</button>
+    <button @click="changeC2">Change C2</button>
+    <button @click="changeCar">Change Car</button>
   </div>
 </template>
 
@@ -18,23 +22,32 @@ defineOptions({
   name: 'Person'
 })
 // data
-let p = reactive({name: 'fiona', age: 35})
+let person = reactive({name: 'fiona', age: 35, car:{c1:'benz', c2:'audi'}})
 
 // function
 function changeName() {
-  p.name +='~'
+  person.name +='~'
 }
 function changeAge() {
-  p.age += 1
+  person.age += 1
+}
+function changeC1() {
+  person.car.c1 = 'yadi'
+}
+function changeC2() {
+  person.car.c2 = 'aima'
+}
+function changeCar() {
+  person.car = {c1:'jiefa', c2:'tongyong'}
 }
 
-function changePerson() {
-  Object.assign(p, {name:'Luna', age:21})
-}
-
-watch(p, (newVal, oldVal) => {
-  console.log(newVal, oldVal)
-})
+// watch(()=>person.name, (newValue, oldValue) => {
+//   console.log(newValue, '--', oldValue)
+//
+// })
+watch(() => person.car, (newValue, oldValue) => {
+   console.log(newValue, '--', oldValue)
+ }, {deep: true})
 
 </script>
 <style>
